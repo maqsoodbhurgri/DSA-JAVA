7e0976bfc9666fd2fa6c96d147a11bcdec431eda
maqsoodbhurgri <maqsoodbhurgri1@gmail.com>
2026-09-04 21:27:17 +0500

    Array print

---
+++ Array/Array.java
@@ -0,0 +1,8 @@
+public class Arrays{
+    public static void main (String[]args){
+        int[]arr = {3,4,2,6,4,6,8};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i] +"");
+        }
+    }
+}
